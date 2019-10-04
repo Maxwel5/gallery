afe7445af2b8729adc0b9bec69b1423674353968
Maxwel5 <maxwellwafulasim@gmail.com>
2019-10-04 14:34:16 +0300

    Add gallery1.apps.Gallery1Config to allow the projects configurations to be reflected in the application

--- galleries/settings.py
+++ galleries/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'gallery1.apps.Gallery1Config',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
