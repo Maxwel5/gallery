ea3415b242092291f26ec2034afec16c8b482ee5
Maxwel5 <maxwellwafulasim@gmail.com>
2019-10-04 16:32:54 +0300

    Load bootstrap create two blocks for css js styling

--- galleries/settings.py
+++ galleries/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'gallery1.apps.Gallery1Config',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
