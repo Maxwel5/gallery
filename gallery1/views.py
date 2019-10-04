b926bc3687048b8a08bef7baa50a23b7a286b2de
Maxwel5 <maxwellwafulasim@gmail.com>
2019-10-04 14:27:41 +0300

    Import Http404 from django.http

--- gallery1/views.py
+++ gallery1/views.py
@@ -1,5 +1,5 @@
 # from django.shortcuts import render
-from django.http  import HttpResponse
+from django.http  import HttpResponse,Http404
 # Create your views here.
 def home(request):
     return HttpResponse('Gallery Shop')
