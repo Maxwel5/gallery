eaef6989b7d346c7bac487ea4dc1a210c1d2695f
Maxwel5 <maxwellwafulasim@gmail.com>
2019-10-04 16:04:40 +0300

    Provide base template to pass logic that will be responsible to display document

--- gallery1/urls.py
+++ gallery1/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    # url(r'^admin/'.admin.site.urls),
     url('^$',views.home),
     # url('^$',views.home,name = 'home'),
     url('^$',views.create_album,name = 'create_album'),
