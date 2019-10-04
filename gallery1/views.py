719b9b95b9ed687264a3343c083f4d779452c99b
Maxwel5 <maxwellwafulasim@gmail.com>
2019-10-04 14:41:43 +0300

    Import render function and pass request and templates file

--- gallery1/views.py
+++ gallery1/views.py
@@ -1,8 +1,11 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.http  import HttpResponse,Http404
 # Create your views here.
+# def home(request):
+#     return HttpResponse('Gallery Shop')
+
 def home(request):
-    return HttpResponse('Gallery Shop')
+    return render(request, 'index.html')
 
 def create_album(request):
     html = f'''
