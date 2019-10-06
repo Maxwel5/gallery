234a7d926557ba71b042213c1dd968a25d75562a
Maxwel5 <maxwellwafulasim@gmail.com>
2019-10-06 22:22:39 +0300

    Import module class and define photo class models

--- gallery1/models.py
+++ gallery1/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Photo(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    # location = models.ForeignKey(Location,on_delete=models.CASCADE) 
+    # category = models.ManyToManyField(categories)
+    image_url = models.ImageField(upload_to="user_images/%Y/%m/%d/")
+    
+    # def __unicode__(self):
+    #     return self.name
--- gallery1/views.py
+++ gallery1/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 from django.http  import HttpResponse,Http404
-# from .models import Image
+from .models import Photo
 
 # Create your views here.
 # def home(request):
 #     return render(request, 'index.html')
 
 def create_album(request):
-    images = Image.all_images()
+    photos = Photo.all_photos()
     locations = Location.objects.all()
     # return HttpResponse(html)
-    return render(request, 'generals/image.html',{"images": images,"locations":locations})
+    return render(request, 'generals/photo.html',{"photos": photos,"locations":locations})
 
 def location(request,location):
     locations = Location.objects.all()
