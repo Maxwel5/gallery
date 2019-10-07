2a8b13759430afe588d2509d30cccc99942918d5
Maxwel5 <maxwellwafulasim@gmail.com>
2019-10-07 04:00:33 +0300

    Creating categories class and define the name to charfield and return it as a string by defining the saving and deleting function

--- gallery1/models.py
+++ gallery1/models.py
@@ -11,13 +11,25 @@ class location(models.Model):
     def save_location(self):
         self.save() 
 
-    def save_location(self):
+    def delete_location(self):
+        self.delete() 
+
+class categories(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    def save_categories(self):
         self.save() 
+
+    def delete_categories(self):
+        self.delete() 
 
 class Photo(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    # location = models.ForeignKey(location,on_delete=models.CASCADE) 
+    location = models.ForeignKey(location,on_delete=models.CASCADE) 
     # category = models.ManyToManyField(categories)
     image_url = models.ImageField(upload_to="user_images/%Y/%m/%d/")
     
