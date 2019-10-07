52192c8757872c34adf34a64dfa9b3f19f70a332
Maxwel5 <maxwellwafulasim@gmail.com>
2019-10-07 08:30:46 +0300

    Deburgging errors

--- gallery1/models.py
+++ gallery1/models.py
@@ -29,8 +29,8 @@ class categories(models.Model):
 class Photo(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    location = models.ForeignKey(location,on_delete=models.CASCADE) 
-    # category = models.ManyToManyField(categories)
+    location = models.ForeignKey(location) 
+    category = models.ManyToManyField(categories)
     image_url = models.ImageField(upload_to="user_images/%Y/%m/%d/")
     
     def __str__(self):
--- gallery1/views.py
+++ gallery1/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render
 from django.http  import HttpResponse,Http404
-from .models import Photo
+from .models import Photo,location
 
 # Create your views here.
 # def home(request):
 #     return render(request, 'index.html')
 
 def create_album(request):
-    photos = Photo.all_photos()
-    locations = Location.objects.all()
+    photos = Photo.objects.all()
+    locations = location.objects.all()
     # return HttpResponse(html)
     return render(request, 'generals/photo.html',{"photos": photos,"locations":locations})
 
 def location(request,location):
-    locations = Location.objects.all()
+    locations = location.objects.all()
     return render(request, 'generals/location.html',{"locations":locations})
