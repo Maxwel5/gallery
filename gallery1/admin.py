000157c04c40ed3fe8aeefe6681cd46464dc4289
Maxwel5 <maxwellwafulasim@gmail.com>
2019-10-07 04:58:00 +0300

    Import model modules from models and register them

--- gallery1/admin.py
+++ gallery1/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Photo, categories,location
 
 # Register your models here.
+admin.site.register(Photo)
+admin.site.register(categories)
+admin.site.register(location)
