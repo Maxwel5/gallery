e8a7a8941ad0bfff2e7bc11ccca91723a1efaa59
Maxwel5 <maxwellwafulasim@gmail.com>
2019-10-07 04:19:46 +0300

    Creating photo and istance test classes and setup method before every test and confirm correct instanciation of objects

--- gallery1/tests.py
+++ gallery1/tests.py
@@ -2,3 +2,12 @@ from django.test import TestCase
 from .models import Photo,categories,location
 
 # Create your tests here.
+class PhotoTestCase(TestCase):
+    # Set Up method
+    def setUp(self):
+        self.food= Photo(name='food',description='The essential requirement for human body to aid in growth and development'image_url='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQw55lGYQ63etgGDG2vcDjOdGEFgoUrNz5h9Qu9bO9_EOq9Vx9M')
+
+    # Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.photo,Photo))
+
