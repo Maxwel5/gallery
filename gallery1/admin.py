6452afde44e731934d478e61edf98ef5a365ee5c
Maxwel5 <maxwellwafulasim@gmail.com>
2019-10-07 09:00:42 +0300

    define a tearDown method to allow deleinge all instances of our models from the database after each test.

--- gallery1/admin.py
+++ gallery1/admin.py
@@ -3,9 +3,9 @@ from .models import Photo, categories,location
 
 # Register your models here.
 
-class categoriesAdmin(admin.ModelAdmin):
-    filter_horizontal =('location',)
+# class categoriesAdmin(admin.ModelAdmin):
+#     filter_horizontal =('location',)
 
 admin.site.register(Photo)
-admin.site.register(categories,categoriesAdmin)
+admin.site.register(categories)
 admin.site.register(location)
