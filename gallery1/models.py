0678899a09333b54391e3e18e07c9c8c86c72f32
Maxwel5 <maxwellwafulasim@gmail.com>
2019-10-07 03:51:04 +0300

    Creating a location class model  defining its function to string, save and delete the location functionality

--- gallery1/models.py
+++ gallery1/models.py
@@ -1,10 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class location(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    def save_location(self):
+        self.save() 
+
+    def save_location(self):
+        self.save() 
+
 class Photo(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    # location = models.ForeignKey(Location,on_delete=models.CASCADE) 
+    # location = models.ForeignKey(location,on_delete=models.CASCADE) 
     # category = models.ManyToManyField(categories)
     image_url = models.ImageField(upload_to="user_images/%Y/%m/%d/")
     
